12e1acb6edda625395c4b6b9a9200c8b17cd052f
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-11 15:29:17 +0530

    create controller and implement password hasing with CURD ops

---
+++ Backend/controller/user.js
@@ -0,0 +1,32 @@
+const {userSchema} = require("../Models/user");
+const bcrypt = require("bcrypt")
+
+async function registerUser(fullName, birthDate, userName, email, password) {
+    try {
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        return await userSchema.create({fullName, birthDate, userName, email, password:hashedPassword});   
+        
+    } catch (error) {
+        console.log("Erorr occured in user controller in registerUser",error);
+    }
+}
+async function loginUserByEmail(email, password) {
+    try {
+        const user =  await userSchema.findOne({email})
+        if(!user){
+            return {status: "Not Found"};
+        }
+        const chkPass =  await bcrypt.compare(password, user.password);
+        if(!chkPass){
+            return {status: "Password Invalid"};
+        }
+        return {status: "Found"};
+
+    } catch (error) {
+        console.log("Error in user controller while finding login Info",error);
+    }
+}
+
+module.exports = {registerUser, loginUser}
