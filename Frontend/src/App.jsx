a6be514e76740db2c5a5310d3f9dcad1606d2675
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-11 22:23:55 +0530

    removed the unused code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const navigate = useNavigate();
 
   return (
-    <>
+    <div>
+      <div className="flex justify-between bg-slate-900 text-white">
+        <div className='m-4 text-xl font-bold'>
+          G1
+        </div>
+        <div className="flex">
+          <button className='m-4 ' onClick={()=>{navigate("/login")}}>
+            Login
+          </button>
+          <button className='m-4 ' onClick={()=>{navigate("/register")}}>
+            Register
+          </button>
+          <button className='m-4 ' onClick={()=>{navigate("/login")}}>
+            About Task
+          </button>
+
+        </div>
+      </div>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='text-5xl font-bold mt-10 '>
+          It's a Demo Site for Project: Pariksha Mitra
+        </div>  
+        <div className='h-1 w-[800px] bg-blue-950 mt-3'></div>
+        <div className='text-xl font-light'>
+        Pariksha Mitra is a comprehensive digital examination platform for Marathi medium students 
+        from 5th to 10th standard.
+        </div>
+
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
