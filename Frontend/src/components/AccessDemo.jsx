24fd4e0c343e63fac8763ceba64f05b09db09490
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-12 00:38:08 +0530

    add basic desing

--- Frontend/src/components/AccessDemo.jsx
+++ Frontend/src/components/AccessDemo.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const AccessDemo = () => {
   return (
     <div>
-      
+      <div className="flex justify-between bg-slate-900 text-white">
+        <div className='m-4 text-xl font-bold'>
+          G1
+        </div>
+        <div className='flex'>
+          <button>Profile</button>
+        </div>
+      </div>
+
+      <div className='flex flex-col justify-center items-center'>
+        <div>Access Controller Demo</div>
+        <div className='flex'>
+        <button>User Access</button>
+        <button>Staff Access</button>
+        <button>Admin Access</button>
+          
+        </div>
+      </div>
     </div>
   )
 }
