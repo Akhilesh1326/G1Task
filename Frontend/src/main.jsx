540d873cd11250eaf6beaba8972e5e320b3053b1
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-11 22:25:12 +0530

    add react-router-dom and add paths for components

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import LoginTypeSelect from './components/LoginTypeSelect.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import AccessDemo from './components/AccessDemo.jsx'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login />},
+  { path: "/register", element: <Register />},
+  { paht: "/AccessDemo", element: <AccessDemo />},
+  { path: "/LoginTypeSelect", element: <LoginTypeSelect />},
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
