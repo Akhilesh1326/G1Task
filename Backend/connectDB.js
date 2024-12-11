58db5a08e35b5903fb16bc2d6bfb6074776193fc
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-12 00:34:34 +0530

    DB name change

--- Backend/connectDB.js
+++ Backend/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 function connectToDB(){
-    mongoose.connect("mongodb://127.0.0.1:27017/G1Task")
+    mongoose.connect("mongodb://127.0.0.1:27017/GOneTask")
     .then(()=>{
         console.log("Connected to DB");
     });
