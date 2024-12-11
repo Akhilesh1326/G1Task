0be1c42e297c3b6c1bfca94b7b4e5e40e8c2bca5
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-12 00:33:20 +0530

    add logic for repeat user data check

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -3,26 +3,30 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     fullName:{
         type:String,
-        require: true,
+        required: true,
     },
     birthDate:{
         type:String,
-        require: true,
+        required: true,
     },
     userName:{
         type: String,
-        require: true,
+        required: true,
         unique: true,
     },
     email:{
         type:String,
-        require: true,
+        required: true,
         unique: true,
     },
     password:{
         type:String,
-        require: true,
+        required: true,
     },
+    role:{
+        type:String,
+        required: true,
+    }
 }, {timestamps: true})
 
 const userSchema = mongoose.model("userSchema", UserSchema);
