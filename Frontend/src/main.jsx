d8a06cb9b467a239bbc212baea22c36f377e5863
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-12 00:31:39 +0530

    add new route for profile component and make corrections in typo

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,15 +5,18 @@ import App from './App.jsx'
 
 import LoginTypeSelect from './components/LoginTypeSelect.jsx'
 import Login from './components/Login.jsx'
+import LoginByEmail from "./components/LoginByEmail.jsx"
 import Register from './components/Register.jsx'
 import AccessDemo from './components/AccessDemo.jsx'
+
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/login", element: <Login />},
+  { path: "/login-username", element: <Login />},
+  { path: "/login-email", element: <LoginByEmail />},
   { path: "/register", element: <Register />},
-  { paht: "/AccessDemo", element: <AccessDemo />},
+  { path: "/accessDemo", element: <AccessDemo />},
   { path: "/LoginTypeSelect", element: <LoginTypeSelect />},
 ])
 
