4429272df0bf0447a4850a84311628e803cf1646
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-14 02:22:39 +0530

    make some UI changes

--- Frontend/src/components/profile.jsx
+++ Frontend/src/components/profile.jsx
@@ -54,7 +54,7 @@ const Profile = () => {
             <span className="text-base text-gray-300 capitalize">{data?.role || 'N/A'}</span>
           </div>
         </div>
-        <button onClick={() => console.log('Edit Profile clicked')} className={`mt-6 w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ${data?.length <= 0 ? "opacity-45 cursor-not-allowed" : ""}`} disabled={data?.length <= 0 ? true : false}>
+        <button onClick={() => console.log('Edit Profile clicked')} className={`mt-6 w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 transform hover:shadow-[0px_0px_7px_1px_#092139]  ${data?.length <= 0 ? "opacity-45 cursor-not-allowed" : ""}`} disabled={data?.length <= 0 ? true : false}>
           Edit Profile
         </button>
       </div>
